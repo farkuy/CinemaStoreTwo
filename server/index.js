6fa5a12e178bd41c02cc17abf3c4083eaae1d717
farkuy <127094746+farkuy@users.noreply.github.com>
2023-06-02 01:49:13 +0300

    create db

---
+++ server/index.js
@@ -0,0 +1,26 @@
+require('dotenv').config();
+const express = require('express');
+const sequelize = require('./db');
+const models = require('./models/models')
+const PORT = process.env.PORT || 5000;
+const cors = require('cors');
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.status(200).json({message: `work`})
+})
+
+
+const start = async () => {
+    try {
+        await sequelize.authenticate()
+        await sequelize.sync()
+        app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
+    } catch (e) {
+        console.log(e)
+    }
+}
+start()
---
+++ server/models/models.js
@@ -0,0 +1,88 @@
+const sequelize = require('../db');
+const {DataTypes} = require('sequelize');
+
+const User = sequelize.define('user', {
+    id: {type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
+    email: {type: DataTypes.STRING, unique: true,},
+    password: {type: DataTypes.STRING},
+    role: {type: DataTypes.STRING, defaultValue: "USER"},
+});
+
+const Basket = sequelize.define('basket', {
+    id: {type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
+});
+
+const BasketContent = sequelize.define('basket_content', {
+    id: {type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
+});
+
+const Content = sequelize.define('content', {
+    id: {type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
+    name: {type: DataTypes.STRING, unique: true, allowNull: false},
+    rating: {type: DataTypes.INTEGER, defaultValue: 0},
+    img: {type: DataTypes.STRING, allowNull: false},
+});
+
+const Genre = sequelize.define('genre', {
+    id: {type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
+    name: {type: DataTypes.STRING, unique: true, allowNull: false},
+});
+
+const MovieStudio = sequelize.define('movie_studio', {
+    id: {type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
+    name: {type: DataTypes.STRING, unique: true, allowNull: false},
+});
+
+const Rating = sequelize.define('rating', {
+    id: {type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
+    rate: {type: DataTypes.INTEGER, allowNull: false},
+});
+
+const ContentInfo = sequelize.define('content_info', {
+    id: {type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
+    title: {type: DataTypes.STRING, allowNull: false},
+    description: {type: DataTypes.STRING, allowNull: false},
+});
+
+const GenreMovieStudio = sequelize.define('genre_movie_studio', {
+    id: {type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
+})
+
+User.hasOne(Basket);
+Basket.belongsTo(User);
+
+User.hasMany(Rating);
+Rating.belongsTo(User);
+
+Basket.hasMany(BasketContent);
+BasketContent.belongsTo(Basket);
+
+Genre.hasMany(Content);
+Content.belongsTo(Genre);
+
+MovieStudio.hasMany(Content);
+Content.belongsTo(MovieStudio);
+
+Content.hasMany(Rating);
+Rating.belongsTo(Content);
+
+Content.hasMany(BasketContent);
+BasketContent.belongsTo(Content);
+
+Genre.belongsToMany(MovieStudio, {through: GenreMovieStudio});
+MovieStudio.belongsToMany(Genre, {through: GenreMovieStudio});
+
+module.exports = {
+    User,
+    Basket,
+    BasketContent,
+    Content,
+    Genre,
+    MovieStudio,
+    Rating,
+    GenreMovieStudio,
+    ContentInfo
+}
+
+
+
